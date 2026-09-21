9df92346ade2bbe073f60872775283cf4f3a7758
MACsepehri <macsepehri@gmail.com>
2026-09-21 22:37:03 +0330

    Base of the website completed, started the main page

---
+++ online-shop/app/layout.jsx
@@ -0,0 +1,9 @@
+import '@/public/css/base.css'
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
