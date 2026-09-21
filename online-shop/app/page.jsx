6574f72fb260c851686604f602e2d8b13425f9c5
MACsepehri <macsepehri@gmail.com>
2026-09-21 23:57:03 +0330

    Updated data.jsx and created the page.jsx (main page) with showing catagories

--- online-shop/app/page.jsx
+++ online-shop/app/page.jsx
@@ -1,6 +1,73 @@
+import { CatagoryBox, CatagoryBoxContent, FlexDiv,
+    MainPageCatagoryProductBox, MainPageCatagoryProductBoxContent,
+    ProductName, ProductDesc, ProductPrice, ProductImage
+ } from "@/public/css/Index"
+import { AllProducts } from "./product-data/data"
+import Link from "next/link"
+
 export default function Home() {
+    
+    let catagories = [
+        'موبایل و تبلت',
+        'لپ تاپ و کامپیوتر',
+        'ایرپاد، هدفون و هنزفری',
+        'ساعت',
+        'تجهیزات گیمینگ'
+    ]
+
+    let en_catagories = [
+        'mobile_and_tablet',
+        'computer',
+        'headphone_and_airpod',
+        'smart_watch',
+        'gaming_objects',
+    ]
+
+    let productAsList = [
+        AllProducts.mobile,
+        AllProducts.computer,
+        AllProducts.headphone,
+        AllProducts.watch,
+        AllProducts.gaming,
+    ]
+
     return (
         <>
+            <div>
+                <h1>به سایت ما خوش آمدید!</h1>
+            </div>
+            <div>
+                {
+                    catagories.map((value,index)=>{
+                        return (
+                            <CatagoryBox key={index}>
+                                <CatagoryBoxContent>
+                                    <h2>{value}</h2>
+                                    <FlexDiv>
+                                        {
+                                            productAsList[index].slice(0,5).map((info)=>{
+                                                return (
+                                                    <Link href={`/product?id=${info.id}`} className="no-decoration-link" key={index}>
+                                                        <MainPageCatagoryProductBox>
+                                                            <MainPageCatagoryProductBoxContent>
+                                                                <ProductImage src={info.image} /><br />
+                                                                <ProductName className="product-name">{info.name}</ProductName><br />
+                                                                <ProductDesc className="product-desc">{info.desc}</ProductDesc><br />
+                                                                <ProductPrice className="product-price">{info.price} تومان</ProductPrice>
+                                                            </MainPageCatagoryProductBoxContent>
+                                                        </MainPageCatagoryProductBox>
+                                                    </Link>
+                                                )
+                                            })
+                                        }
+                                    </FlexDiv>
+                                    <br />
+                                </CatagoryBoxContent>
+                            </CatagoryBox>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
---
+++ online-shop/public/css/Index.jsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const CatagoryBox = styled.div`
+    width: 100%;
+    margin-bottom: 30px;
+    display: flex;
+    justify-content: flex-start;
+    border-bottom: 2px solid #e3e3e39a;
+`;
+
+export const CatagoryBoxContent = styled.div`
+    margin-right: 30px;
+`
+export const FlexDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+export const MainPageCatagoryProductBox = styled.div`
+    width: 300px;
+    height: 300px;
+    border-radius: 10px;
+    border: 2px solid #e0e0e0;
+    background: #eeeeee;
+    margin-right: 10px;
+    margin-left: 10px;
+    position: relative;
+    overflow: hidden;
+`
+
+export const MainPageCatagoryProductBoxContent = styled.div`
+    margin-top: 20%;
+    margin-right: 0%;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    box-sizing: border-box;
+`
+
+export const ProductName = styled.span``
+export const ProductDesc = styled.span`
+    font-size: 14px;
+    color: black;
+`
+export const ProductPrice = styled.span`
+    position: absolute;
+    bottom: 10px;
+    justify-content: center;
+    text-align: center;
+    margin: auto;
+    right: 50%;
+    transform: translateX(50%)
+`
+export const ProductImage = styled.img`
+    max-width: 128px;
+    max-height: 128px;
+    margin-right: 25%;
+    margin-top: -70px;
+    object-fit: contain;
+    mix-blend-mode: multiply;
+`;
